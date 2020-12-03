bfd5f9095277b2c6f6868b55310252a675f225ee
areThereAnyUserNamesLeft <r1ch4rdp4p3@gmail.com>
2020-12-03 07:30:59 +0000

    tidying before remote push

--- database/main.go
+++ database/main.go
@@ -6,77 +6,106 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongodb:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
-
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 	db := client.Database("swapi")
+	err = InputData(ctx, db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// InputData Safely puts the swapi data into the DB
+func InputData(ctx context.Context, db *mongo.Database) error {
 	var films structs.Films
 	filmsC := ReadIn("films", "./database/data/films.json", &films, db)
 	for _, f := range films {
 		ir, err := filmsC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Title, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Title, ir.InsertedID)
 	}
 	var people structs.People
 	peopleC := ReadIn("people", "./database/data/people.json", &people, db)
 	for _, f := range people {
 		ir, err := peopleC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Name, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Name, ir.InsertedID)
 	}
 	var planets structs.Planets
 	planetsC := ReadIn("planets", "./database/data/planets.json", &planets, db)
 	for _, f := range planets {
 		ir, err := planetsC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Name, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Name, ir.InsertedID)
 	}
 	var species structs.Species
 	speciesC := ReadIn("species", "./database/data/species.json", &species, db)
 	for _, f := range species {
 		ir, err := speciesC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Name, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Name, ir.InsertedID)
 	}
 	var starships structs.Starships
-	starshipsC := ReadIn("startships", "./database/data/Starships.json", &starships, db)
+	starshipsC := ReadIn("startships", "./database/data/starships.json", &starships, db)
 	for _, f := range starships {
 		ir, err := starshipsC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Pilots, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Pilots, ir.InsertedID)
 	}
 	var vehicles structs.Vehicles
 	vehiclesC := ReadIn("vehicles", "./database/data/vehicles.json", &vehicles, db)
 	for _, f := range vehicles {
 		ir, err := vehiclesC.InsertOne(ctx, f)
 		if err != nil {
-			panic(err)
+			fmt.Printf("%s - %s\n", f.Fields.Pilots, "Insert Error")
+			return err
 		}
-		fmt.Println(ir.InsertedID)
+		fmt.Printf("%s - %s\n", f.Fields.Pilots, ir.InsertedID)
 	}
+	return nil
 }
 
 // ReadIn takes a jsonfile and a matching empty interface and fills it with data.
